ac4e328f8e13218b45b24cdd396136f7edfecd0d
iliyaSRblack <ilia.salim.bayati@gmail.com>
2026-02-07 18:03:25 +0330

    add vieiws

--- sabzeno/views.py
+++ sabzeno/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categoris = Category.objects.all()
+    products = Product.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    context = {
+        'category': category,
+        'categoris': categoris,
+        'products': products,
+    }
+    return render(request, 'sabzeno/shop/list-products.html', context)
